2da3e94f9683103ae3ab7d1342ddfcedf45a7665
EffectoftheMind <39631708+EffectoftheMind@users.noreply.github.com>
2018-05-29 13:19:23 -0500

    Create learning_turtle.py

---
+++ learning_turtle.py
@@ -0,0 +1,58 @@
+import turtle 
+wn = turtle.Screen()         
+wn.bgcolor("black")                  
+bob = turtle.Turtle()      
+
+bob.speed("fastest")
+bob.shape("turtle")    
+bob.pencolor("#78FFFD")        
+
+for x in range(4):
+  bob.circle(100)      
+  bob.right(90)          
+
+bob.pencolor("#F115FB")          
+for x in range(4):
+  bob.circle(90)       
+  bob.right(90)      
+  
+bob.pencolor("#15FB34")
+for x in range(4):
+  bob.circle(80)
+  bob.right(90)
+  
+bob.pencolor("#843FE1")
+for x in range(4):
+  bob.circle(70)
+  bob.right(90)
+  
+bob.pencolor("#E99B00")
+for x in range(4):
+  bob.circle(60)
+  bob.right(90)
+  
+bob.pencolor("#FFFF07")
+for x in range(4):
+  bob.circle(50)
+  bob.right(90)
+  
+bob.pencolor("#FF0778")
+for x in range(4):
+  bob.circle(40)
+  bob.right(90)
+  
+bob.pencolor("#31FFEF")
+for x in range(4):
+  bob.circle(30)
+  bob.right(90)
+  
+bob.pencolor("#C8FF36")
+for x in range(4):
+  bob.circle(20)
+  bob.right(90)
+  
+bob.pencolor("#19F5D3")
+for x in range(4):
+  bob.circle(10)
+  bob.right(90)
+  
